81fbf822856fbdacaeacc8e65cfb4d396045db47
Jason Maurer <jason@maur.co>
2019-11-07 17:42:01 -0700

    Take deleted files into account when invalidating

--- s3-deploy/entrypoint.sh
+++ s3-deploy/entrypoint.sh
@@ -7,8 +7,6 @@ BUCKET_NAME=${2-}
 CLOUDFRONT_ID=${3-}
 INVALIDATE_ALL=${4-}
 
-FILES=()
-
 if [ -n "${GITHUB_WORKSPACE-}" ]; then
 	cd $GITHUB_WORKSPACE
 fi
@@ -18,15 +16,34 @@ if [ -n "${DEPLOY_DIR-}" ]; then
 fi
 
 aws s3 sync --delete . s3://$BUCKET_NAME | {
-	while read -r i;
+	# List of files that are being uploaded or deleted
+	FILES=()
+
+	# Extract filename from s3 sync output
+	while read -r line;
  	do
-		FILES+=("/$(echo $i |\
- 			sed -E "s/.*upload: (.*) to.*/\1/" |\
- 			sed -E "s/^.\///" |\
- 			sed -E "s/\/index.html/\//")")
+ 		PARSED=$(echo $line |\
+ 			# Get full operation path
+			sed -E "s/.*(upload|delete): (.*)/\2/" |\
+			# Remove current directory slash
+			sed -E "s/^.\///" |\
+			# Get destination filename
+			sed -E "s/.*s3:\/\/$BUCKET_NAME\/(.*)/\1/")
+		FILES+=("/$PARSED")
 	done
 
+	for file in "${FILES[@]}"; do
+		# Add an invalidation path for index routes without "index.html"
+		# e.g. invalidate both /about/index.html and /about/
+		if [[ "$file" =~ \/index\.html$ ]]; then
+			PARSED=$(echo $file | sed -E "s/index.html//")
+			FILES+=("$PARSED")
+		fi
+	done
+
+	# Run invalidation if any files were uploaded or deleted
 	if [ -n "${CLOUDFRONT_ID-}" ] && [ ${#FILES[@]} -gt 0 ]; then
+		# Create string from filelist that can be passed as argument
 		if [ "${INVALIDATE_ALL-}" != "true" ]; then
 			PATHS=$(printf "%q " "${FILES[@]}")
 		fi
