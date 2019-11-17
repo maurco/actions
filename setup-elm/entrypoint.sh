0565fd62759331024ca5671126f6db25660a270c
Jason Maurer <jason@maur.co>
2019-11-16 19:09:39 -0700

    Add bin path

--- setup-elm/entrypoint.sh
+++ setup-elm/entrypoint.sh
@@ -2,9 +2,12 @@
 
 set -Eeuo pipefail
 
-wget -qO elm.gz https://github.com/elm/compiler/releases/download/${INPUT_ELM_VERSION}/binary-for-linux-64-bit.gz
-gunzip elm.gz
-chmod +x elm
-mv elm /usr/local/bin
+BIN_FILE=/bin/elm
 
+wget -qO- https://github.com/elm/compiler/releases/download/${INPUT_ELM_VERSION}/binary-for-linux-64-bit.gz |\
+	gunzip -c > $BIN_FILE
+
+chmod +x $BIN_FILE
+
+echo "::add-path::$BIN_FILE"
 echo "Elm ${INPUT_ELM_VERSION} installed!"
