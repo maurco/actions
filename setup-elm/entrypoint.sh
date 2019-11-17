833ef1a4af92358cd97a133e44ca5d22ade07008
Jason Maurer <jason@maur.co>
2019-11-16 18:57:11 -0700

    Problems with folder permissions?

--- setup-elm/entrypoint.sh
+++ setup-elm/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -Eeuo pipefail
 
-BIN_FILE=/usr/local/bin/elm
+wget -qO elm.gz https://github.com/elm/compiler/releases/download/${INPUT_ELM_VERSION}/binary-for-linux-64-bit.gz
+gunzip elm.gz
+chmod +x elm
+mv elm /usr/local/bin
 
-wget -qO- https://github.com/elm/compiler/releases/download/${INPUT_ELM_VERSION}/binary-for-linux-64-bit.gz |\
-	gunzip -c > $BIN_FILE
-
-chmod +x $BIN_FILE
+echo "Elm ${INPUT_ELM_VERSION} installed!"
