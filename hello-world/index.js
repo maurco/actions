ac418fdd4f66ff68dce2228471acafa807c3824c
Jason Maurer <jason@maur.co>
2019-11-20 21:34:20 -0700

    Figure out where we are

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,9 @@
-const { spawn } = require("child_process")
+const fs = require("fs")
+// const { spawn } = require("child_process")
 // spawn("./hello-world", [], { stdio: "inherit" })
-spawn("pwd && ls -a", [], { stdio: "inherit" })
-spawn("cd .. && ls -a", [], { stdio: "inherit" })
+// spawn("pwd && ls -a", [], { stdio: "inherit" })
+// spawn("cd .. && ls -a", [], { stdio: "inherit" })
+
+console.log(process.cwd())
+console.log(__dirname)
+console.log(fs.readdirSync(__dirname))
