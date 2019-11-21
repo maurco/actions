7383f6cd7f3e20d818afbaa9d9fcbf76e52116b8
Jason Maurer <jason@maur.co>
2019-11-20 21:54:46 -0700

    Okay, is this good enough?

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,9 +1,2 @@
-const fs = require("fs")
-// const { spawn } = require("child_process")
-// spawn("./hello-world", [], { stdio: "inherit" })
-// spawn("pwd && ls -a", [], { stdio: "inherit" })
-// spawn("cd .. && ls -a", [], { stdio: "inherit" })
-
-console.log(process.cwd())
-console.log(__dirname)
-console.log(fs.readdirSync(__dirname))
+const { spawn } = require("child_process")
+spawn(`${__dirname}/target/release/hello-world`, [], { stdio: "inherit" })
