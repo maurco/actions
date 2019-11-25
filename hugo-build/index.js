60f0f258c6c0ba4e81f9c16b687bd929a4af53ec
Jason Maurer <jason@maur.co>
2019-11-24 19:40:39 -0700

    Ugh.

--- hugo-build/index.js
+++ hugo-build/index.js
@@ -1,2 +1,6 @@
+const fs = require("fs")
 const { spawn } = require("child_process")
+
+console.log(fs.readdirSync(__dirname))
+console.log(fs.readdirSync(`${__dirname}/bin`))
 spawn(`${__dirname}/bin/hugo-build`, { stdio: "inherit" })
