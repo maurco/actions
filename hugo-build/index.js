b6c51a97276985b43072aa3bb3e24eb78fd18c02
Jason Maurer <jason@maur.co>
2019-11-26 19:25:40 -0700

    Test calling executable with node

--- hugo-build/index.js
+++ hugo-build/index.js
@@ -2,7 +2,11 @@ const { spawn } = require("child_process")
 
 process.chdir(`${__dirname}/../`)
 console.log(process.cwd())
+spawn("ls -a", { stdio: "inherit" })
 
-spawn("ls", { stdio: "inherit" })
+process.chdir("../")
+console.log(process.cwd())
+spawn("ls -a", { stdio: "inherit" })
+
 spawn("git", ["lfs", "pull"], { stdio: "inherit" })
 spawn("hugo-build/bin/hugo-build", { stdio: "inherit" })
