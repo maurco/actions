412c021cd2a52efe4c095e43afbabed460035b00
Jason Maurer <jason@maur.co>
2019-11-26 17:14:30 -0700

    Add actions folder for reusable functions

---
+++ actions/src/main.go
@@ -0,0 +1,35 @@
+package actions
+
+import (
+	"fmt"
+	"os"
+)
+
+func ChangeDirByEnvVar(key string) {
+	if val, ok := os.LookupEnv(key); ok && val != "" {
+		err := os.Chdir(val)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func AddFlagByEnvVar(flags *[]string, flagFormat int, name, key string) {
+	var format string
+	switch flagFormat {
+	case 1:
+		format = "--%s=%s"
+	case 2:
+		format = "--%s %s"
+	case 3:
+		format = "-%s=%s"
+	case 4:
+		format = "-%s %s"
+	default:
+		panic(fmt.Sprintf("AddFlagByEnvVar{flagFormat} must be an int between 1-4, got %d", flagFormat))
+	}
+
+	if val, ok := os.LookupEnv(key); ok && val != "" {
+		*flags = append(*flags, fmt.Sprintf(format, name, val))
+	}
+}
---
+++ actions/src/main_test.go
@@ -0,0 +1,44 @@
+package actions
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChangeDirByEnvVar(t *testing.T) {
+	d, _ := os.Getwd()
+	os.Chdir("..")
+
+	cwd, _ := os.Getwd()
+	assert.NotEqual(t, cwd, d)
+
+	os.Setenv("FOOBAR", "src")
+	ChangeDirByEnvVar("FOOBAR")
+
+	cwd, _ = os.Getwd()
+	assert.Equal(t, cwd, d)
+	assert.Panics(t, func() {
+		os.Setenv("FOOBAR", "does-not-exist")
+		ChangeDirByEnvVar("FOOBAR")
+	})
+}
+
+func TestAddFlagByEnvVar(t *testing.T) {
+	var flags []string
+	os.Setenv("FOOBAR", "bar")
+
+	AddFlagByEnvVar(&flags, 1, "foo", "FOOBAR")
+	AddFlagByEnvVar(&flags, 2, "foo", "FOOBAR")
+	AddFlagByEnvVar(&flags, 3, "foo", "FOOBAR")
+	AddFlagByEnvVar(&flags, 4, "foo", "FOOBAR")
+
+	assert.Equal(t, flags[0], "--foo=bar")
+	assert.Equal(t, flags[1], "--foo bar")
+	assert.Equal(t, flags[2], "-foo=bar")
+	assert.Equal(t, flags[3], "-foo bar")
+	assert.Panics(t, func() {
+		AddFlagByEnvVar(&flags, 5, "foo", "FOOBAR")
+	})
+}
