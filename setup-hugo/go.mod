54b54efaaa541056a5d00d36f5c5b761c177f435
Jason Maurer <jason@maur.co>
2019-11-28 01:14:51 -0700

    Use CacheBin with hugo

--- setup-hugo/go.mod
+++ setup-hugo/go.mod
@@ -2,4 +2,4 @@ module github.com/maurerlabs/actions/setup-hugo
 
 go 1.13
 
-require github.com/maurerlabs/actions/toolkit v0.0.0-20191128080032-bc83e3e67906
+require github.com/maurerlabs/actions/toolkit v0.0.0-20191128081312-ec3a82076e09
--- setup-hugo/src/main.go
+++ setup-hugo/src/main.go
@@ -11,7 +11,7 @@ const LATEST_VERSION = "0.60.0"
 func main() {
 	var (
 		version  = toolkit.GetInput("version", &toolkit.InputOptions{Fallback: LATEST_VERSION})
-		extended = toolkit.GetInput("extended", &toolkit.InputOptions{Fallback: "true"}) == "true"
+		extended = toolkit.GetInput("extended", &toolkit.InputOptions{Fallback: "false"}) == "true"
 	)
 
 	if extended {
@@ -20,18 +20,17 @@ func main() {
 		toolkit.Info("Downloading Hugo")
 	}
 
-	filename := "hugo"
+	name := "hugo"
 	if extended {
-		filename = "hugo_extended"
+		name = "hugo_extended"
 	}
 
-	url := fmt.Sprintf("https://github.com/gohugoio/hugo/releases/download/v%s/%s_%s_Linux-64bit.tar.gz", version, filename, version)
-	name := fmt.Sprintf("hugo%s", filename)
-
+	url := fmt.Sprintf("https://github.com/gohugoio/hugo/releases/download/v%s/%s_%s_Linux-64bit.tar.gz", version, name, version)
 	extracted := toolkit.ExtractTar(toolkit.DownloadFile(url))
 	cache := toolkit.CacheTool(extracted, name, version, "amd64")
 
-	toolkit.AddPath(cache)
 	toolkit.MakeExecutable(cache, "hugo")
+	toolkit.CacheBin(cache, "hugo")
+
 	toolkit.Command("hugo", "version")
 }
